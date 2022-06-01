cb2e575fb6d4f77847ddb55b6e789e55865bc13d
decosawa <62211442+andytxr@users.noreply.github.com>
2022-06-01 16:07:53 -0300

    Utilizando funções anônimas

---
+++ functions/anonFunction.js
@@ -0,0 +1,31 @@
+const sum = function(x, y){
+
+    return x+y
+
+}
+
+const results = function(a, b, op = sum){
+
+    console.log(op(a, b));
+
+}
+
+results(3, 4);
+results(3, 4, sum);
+results(3, 4, function(x,y){
+
+    return x-y
+
+})
+results(3, 4, (x, y)=>x*y);
+const people = {
+
+    talk: function (){
+
+        console.log('Oi');
+
+    }
+
+}
+
+people.talk()
